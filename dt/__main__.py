6175dd4d0033b20ef5ec419139e5904557bd26cf
Luca Cotti <lucacotti@outlook.com>
2024-01-21 14:50:15 +0100

    Adds .env config, moves json files

--- dt/__main__.py
+++ dt/__main__.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
+import os
+import tomllib
 import typer
 import uvicorn
 
 from api import create_api
 from data import JSONRepository
 from plots import plot_consumptions_matrix, plot_simulated_matrix
-import const
+
+with open(os.environ["CONFIG_FILE"], "rb") as f:
+    config = tomllib.load(f)
+
+if config["database"]["type"] != "json":
+    raise ValueError("Database type not supported")
+
+repository = JSONRepository(config["database"]["appliances_dir"], config["database"]
+                            ["routines_dir"], config["database"]["test_routines_dir"])
 
 app = typer.Typer()
-repository = JSONRepository(
-    const.APPLIANCES_DIR, const.ROUTINES_DIR, const.TEST_ROUTINES_DIR)
 
 
 @app.command()
--- dt/const.py
+++ dt/const.py
@@ -1,4 +1,1 @@
 MINUTES_IN_DAY = 24 * 60
-APPLIANCES_DIR = "dt/json/appliances"
-ROUTINES_DIR = "dt/json/routines"
-TEST_ROUTINES_DIR = "dt/json/test_routines"
