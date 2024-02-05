b471dce4221303c2490cb4b8f4485a94f20704f8
Luca Cotti <lucacotti@outlook.com>
2024-02-06 00:35:12 +0100

    Migrates to yarn because npm is bugged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./frontend/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./frontend/**/*.{html,js,jsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin"), require("flowbite-typography"),],
+  plugins: [require("flowbite/plugin"), require("flowbite-typography")],
 };
